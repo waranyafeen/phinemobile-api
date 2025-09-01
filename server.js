cdbef80dab13d8d7c333036d0890940b49b2e09c
waranyafeen <144030485+waranyafeen@users.noreply.github.com>
2025-09-01 21:54:34 +0700

    create route api user company buy

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 //Controllers
 //
 const { UserController } = require("./controllers/UserController");
+const { CompanyController } = require("./controllers/CompanyContoller");
+const { ProductController } = require("./controllers/PoductController");
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +19,23 @@ app.get("/", (req, res) => {
 });
 
 //Routes
+
+//
+//User
+//
 app.post("/api/user/signin", UserController.signIn);
+
+//
+//Company
+//
+app.post("/api/company/create", CompanyController.create);
+app.get("/api/company/list", CompanyController.list);
+
+//
+//buy
+//
+app.post("/api/buy/create", ProductController.create);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
