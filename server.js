7caed03da3626e45e8a0c7ea7339f0b3a47588d8
waranyafeen <144030485+waranyafeen@users.noreply.github.com>
2025-09-05 00:45:55 +0700

    update route of user and sell

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const { UserController } = require("./controllers/UserController");
 const { CompanyController } = require("./controllers/CompanyContoller");
 const { ProductController } = require("./controllers/PoductController");
+const { SellController } = require("./controllers/SellController");
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +25,8 @@ app.get("/", (req, res) => {
 //User
 //
 app.post("/api/user/signin", UserController.signIn);
+app.get("/api/user/info", UserController.info); 
+app.put("/api/user/update/", UserController.update);
 
 //
 //Company
@@ -38,6 +41,14 @@ app.post("/api/buy/create", ProductController.create);
 app.get("/api/buy/list", ProductController.list); //กรณีเป็นสินค้ามือสอง
 app.put("/api/buy/update/:id", ProductController.update);
 app.delete("/api/buy/remove/:id", ProductController.remove); //ลบแบบ soft delete
+
+//
+//sell
+//
+app.post("/api/sell/create", SellController.create);
+app.get("/api/sell/list", SellController.list);
+app.delete("/api/sell/remove/:id", SellController.remove);
+app.get("/api/sell/confirm", SellController.confirm);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
