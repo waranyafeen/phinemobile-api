eaeae3bee56bc36ad359a9bf40bc64637c2925cc
waranyafeen <144030485+waranyafeen@users.noreply.github.com>
2025-09-13 15:23:38 +0700

    create route export, dashboard, history, info

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ const { ProductController } = require("./controllers/PoductController");
 const { SellController } = require("./controllers/SellController");
 const { ServiceController } = require("./controllers/ServiceController");
 
+
+//
+// middleware
+//
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/uploads', express.static('uploads'));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -40,12 +45,13 @@ app.post("/api/company/create", CompanyController.create);
 app.get("/api/company/list", CompanyController.list);
 
 //
-//buy
+//buy - product
 //
 app.post("/api/buy/create", ProductController.create);
-app.get("/api/buy/list", ProductController.list); //กรณีเป็นสินค้ามือสอง
+app.get("/api/buy/list/:page", ProductController.list); //กรณีเป็นสินค้ามือสอง
 app.put("/api/buy/update/:id", ProductController.update);
 app.delete("/api/buy/remove/:id", ProductController.remove); //ลบแบบ soft delete
+app.post("/api/buy/export", ProductController.exportToExcel);
 
 //
 //sell
@@ -54,7 +60,9 @@ app.post("/api/sell/create", SellController.create);
 app.get("/api/sell/list", SellController.list);
 app.delete("/api/sell/remove/:id", SellController.remove);
 app.get("/api/sell/confirm", SellController.confirm);
-app.get("/api/sell/dashboard", SellController.dashboard);
+app.get("/api/sell/dashboard/:year", SellController.dashboard);
+app.get("/api/sell/history", SellController.history);
+app.get("/api/sell/info/:id", SellController.info);
 
 //
 //service
